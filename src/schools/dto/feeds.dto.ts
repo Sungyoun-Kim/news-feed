69fe24739fa5613a20cb809b4ad7fe981a81ae3d
kim sungyoun <ksungyoun@software4u.co.kr>
2023-07-08 14:39:08 +0900

    school 관련 API 스웨거 작성

--- src/schools/dto/feeds.dto.ts
+++ src/schools/dto/feeds.dto.ts
@@ -1,22 +1,33 @@
 import { IsOptional, IsString } from 'class-validator';
 import { School } from '../interface/schools.interface';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSchoolFeedDto {
   id: string;
   school: School;
 
+  @ApiProperty({ description: '제목', example: '페이지 생성 후 첫 소식' })
   @IsString()
   subject: string;
 
+  @ApiProperty({ description: '내용', example: '페이지 생성 후 첫 소식이에요' })
   @IsString()
   content: string;
 }
 
 export class UpdateSchoolFeedDto {
+  @ApiProperty({
+    description: '제목',
+    example: '수정하고싶은 제목',
+  })
   @IsOptional()
   @IsString()
   subject?: string;
 
+  @ApiProperty({
+    description: '내용',
+    example: '수정하고싶은 내용',
+  })
   @IsOptional()
   @IsString()
   content?: string;
--- src/schools/schools.controller.ts
+++ src/schools/schools.controller.ts
@@ -15,11 +15,42 @@ import { SchoolsService } from './schools.service';
 import { CreateSchoolPageDto } from './dto/schools.dto';
 import { Request, Response } from 'express';
 import { CreateSchoolFeedDto, UpdateSchoolFeedDto } from './dto/feeds.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNoContentResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('School')
 @Controller('schools')
 export class SchoolsController {
   constructor(private readonly schoolService: SchoolsService) {}
 
+  @ApiOperation({
+    summary: '학교 페이지 생성',
+    description:
+      '- 학교 관리자는 지역, 학교명으로 학교 페이지를 생성할 수 있다\n',
+  })
+  @ApiBody({
+    description: '- 학교 페이지 생성 시 필요한 데이터',
+    type: CreateSchoolPageDto,
+  })
+  @ApiUnauthorizedResponse({
+    description: '- 요청이 인가되지 않은 경우',
+  })
+  @ApiCreatedResponse({
+    description: '- 학교 페이지 생성 성공 ',
+  })
+  @ApiBadRequestResponse({
+    description:
+      '- 지역이 존재하지 않는 경우\n' + '- 올바르지 못한 값이 전달된 경우',
+  })
   @Post()
   async createSchoolPage(
     @Req() req: Request,
@@ -39,10 +70,38 @@ export class SchoolsController {
     res.status(HttpStatus.CREATED).json('create school page successfully');
   }
 
-  @Post(':id/feeds')
+  @ApiOperation({
+    summary: '학교 페이지에 피드 생성',
+    description: '- 학교 관리자는 학교 페이지 내에 소식을 작성할 수 있다\n',
+  })
+  @ApiParam({
+    name: 'schoolId',
+    description: '피드를 생성하려는 학교의 id',
+    required: true,
+  })
+  @ApiBody({
+    description: '- 학교 페이지의 피드 생성 시 필요한 데이터',
+    type: CreateSchoolFeedDto,
+  })
+  @ApiCreatedResponse({
+    description: '- 학교 페이지 내에 피드 생성 성공 ',
+  })
+  @ApiBadRequestResponse({
+    description:
+      '- schoolId를 가진 학교가 존재하지 않는 경우 \n' +
+      '- 올바르지 못한 값이 전달된 경우',
+  })
+  @ApiUnauthorizedResponse({
+    description: '- 요청이 인가되지 않은 경우',
+  })
+  @ApiForbiddenResponse({
+    description:
+      '- 피드 생성을 시도한 유저가 schoolId를 가진 학교의 관리자가 아닌 경우\n',
+  })
+  @Post(':schoolId/feeds')
   async createSchoolFeed(
     @Req() req: Request,
-    @Param('id') schoolId: string,
+    @Param('schoolId') schoolId: string,
     @Body() createSchoolFeedDto: CreateSchoolFeedDto,
     @Res() res: Response,
   ) {
@@ -59,6 +118,37 @@ export class SchoolsController {
     res.status(HttpStatus.CREATED).json('create school feed successfully');
   }
 
+  @ApiOperation({
+    summary: '학교 페이지에 피드 삭제',
+    description:
+      '- 학교 관리자는 학교 페이지 내에 작성된 소식을 삭제할 수 있다\n',
+  })
+  @ApiParam({
+    name: 'schoolId',
+    description: '피드를 삭제하려는 학교의 id',
+    required: true,
+  })
+  @ApiParam({
+    name: 'feedId',
+    description: '삭제하려는 피드의 id',
+    required: true,
+  })
+  @ApiNoContentResponse({
+    description: '- 학교 페이지 내에 피드 삭제 성공 ',
+  })
+  @ApiBadRequestResponse({
+    description:
+      '- schoolId를 가진 학교가 존재하지 않는 경우 \n' +
+      '- feedId를 가진 피드가 존재하지 않는 경우 \n' +
+      '- 올바르지 못한 값이 전달된 경우',
+  })
+  @ApiUnauthorizedResponse({
+    description: '- 요청이 인가되지 않은 경우',
+  })
+  @ApiForbiddenResponse({
+    description:
+      '- 피드 삭제를 시도한 유저가 schoolId를 가진 학교의 관리자가 아닌 경우\n',
+  })
   @Delete(':schoolId/feeds/:feedId')
   async deleteSchoolFeed(
     @Req() req: Request,
@@ -85,6 +175,41 @@ export class SchoolsController {
     res.status(HttpStatus.NO_CONTENT).end();
   }
 
+  @ApiOperation({
+    summary: '학교 페이지에 피드 수정',
+    description:
+      '- 학교 관리자는 학교 페이지 내에 작성된 소식을 수정할 수 있다\n',
+  })
+  @ApiParam({
+    name: 'schoolId',
+    description: '피드를 수정하려는 학교의 id',
+    required: true,
+  })
+  @ApiParam({
+    name: 'feedId',
+    description: '수정하려는 피드의 id',
+    required: true,
+  })
+  @ApiBody({
+    description: '수정할 내용에 대한 데이터',
+    type: UpdateSchoolFeedDto,
+  })
+  @ApiNoContentResponse({
+    description: '- 학교 페이지 내에 피드 수정 성공 ',
+  })
+  @ApiBadRequestResponse({
+    description:
+      '- schoolId를 가진 학교가 존재하지 않는 경우 \n' +
+      '- feedId를 가진 피드가 존재하지 않는 경우 \n' +
+      '- 올바르지 못한 값이 전달된 경우',
+  })
+  @ApiUnauthorizedResponse({
+    description: '- 요청이 인가되지 않은 경우',
+  })
+  @ApiForbiddenResponse({
+    description:
+      '- 피드 수정을 시도한 유저가 schoolId를 가진 학교의 관리자가 아닌 경우\n',
+  })
   @Patch(':schoolId/feeds/:feedId')
   async updateSchoolFeed(
     @Req() req: Request,
