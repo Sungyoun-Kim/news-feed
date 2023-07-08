6609b1cad8900ea076e803024e44f4f84e0d3256
kim sungyoun <ksungyoun@software4u.co.kr>
2023-07-08 19:36:43 +0900

    권한 검사 가드 구현

---
+++ src/auth/guard/role.guard.ts
@@ -0,0 +1,37 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  SetMetadata,
+} from '@nestjs/common';
+
+import { Reflector } from '@nestjs/core';
+import { Role } from 'src/users/schema/users.schema';
+
+export const RoleLevel = (roleLevel: Role) =>
+  SetMetadata('roleLevel', roleLevel);
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const allowUnauthorizedRequest = this.reflector.get<boolean>(
+      'allowUnauthorizedRequest',
+      context.getHandler(),
+    );
+
+    if (allowUnauthorizedRequest) {
+      return true;
+    }
+    // API에 설정된 roleLevel 가져오기
+    const roleLevel = this.reflector.get<Role>(
+      'roleLevel',
+      context.getHandler(),
+    );
+    const req: Express.Request = context.switchToHttp().getRequest();
+    const { user } = req;
+
+    return user.role >= roleLevel;
+  }
+}
