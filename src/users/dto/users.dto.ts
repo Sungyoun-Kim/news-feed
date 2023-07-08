51bfe0970cdc0b6cb47b0da7736e87bc9ee6b469
kim sungyoun <ksungyoun@software4u.co.kr>
2023-07-08 13:51:49 +0900

    유저 회원가입 스웨거 작성

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,16 +1,29 @@
 import { IsEmail, IsEnum, IsString } from 'class-validator';
 
 import { Role } from '../schema/users.schema';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignUpUserDto {
   id: string;
 
+  @ApiProperty({
+    description: '가입하고자 하는 이메일',
+    example: 'test2@email.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: '가입하고자 하는 비밀번호',
+    example: '1234',
+  })
   @IsString()
   password: string;
 
+  @ApiProperty({
+    description: '가입하고자 하는 유저의 역할',
+    enum: Role,
+  })
   @IsEnum(Role)
   role: number;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,11 +10,35 @@ import { UsersService } from './users.service';
 import { SignUpUserDto } from './dto/users.dto';
 import { Response } from 'express';
 import { AllowUnauthorizedRequest } from '../auth/guard/jwt-auth.guard';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
+  @ApiOperation({
+    summary: '회원가입',
+    description:
+      '- 동일한 email이 없을 시, email, password를 가진 회원을 생성한다\n',
+  })
+  @ApiBody({
+    description: '- 회원가입시 필요한 데이터',
+    type: SignUpUserDto,
+  })
+  @ApiCreatedResponse({
+    description: '- 회원가입 성공 ',
+  })
+  @ApiBadRequestResponse({
+    description:
+      '- 이메일이 이미 존재하는 경우\n' + '- 올바르지 못한 값이 전달된 경우',
+  })
   @AllowUnauthorizedRequest()
   @Post('sign-up')
   async signUpUser(@Body() signUpUserDto: SignUpUserDto, @Res() res: Response) {
